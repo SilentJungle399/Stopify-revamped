1b362741258c6653982a275174295f791647a724
SilentJungle399 <silentjungle399@gmail.com>
2023-06-05 12:19:03 +0200

    stream impl using blobs

--- app.vue
+++ app.vue
@@ -1,9 +1,52 @@
 <template>
 	<div>hi</div>
+	<button @click="play">test</button>
+	<audio id="testaudio" controls preload="none"></audio>
 </template>
 
 <script setup lang="ts">
 const { $io } = useNuxtApp();
 
-onMounted(() => {});
+async function play() {
+	$io.emit("songRequest", "https://www.youtube.com/watch?v=ox4tmEV6-QU");
+
+	const bufferChunks: ArrayBuffer[] = [];
+	const audio = document.getElementById("testaudio") as HTMLAudioElement;
+	audio.volume = 0.5;
+	const songMetadata = ref({});
+	const playableBlob = ref<Blob>();
+
+	$io.on("songMetadata", (data) => {
+		songMetadata.value = data;
+	});
+
+	$io.on("songData", (data) => {
+		bufferChunks.push(data);
+
+		if (bufferChunks.length === 5) {
+			const blob = new Blob(bufferChunks);
+			const chunkDuration = getDuration(bufferChunks, songMetadata.value);
+			const url = URL.createObjectURL(blob);
+			audio.src = url;
+			audio.play();
+
+			audio.addEventListener("ended", () => {
+				const blob = new Blob(bufferChunks);
+				const url = URL.createObjectURL(blob);
+				audio.src = url;
+				audio.currentTime = chunkDuration;
+				audio.play();
+			});
+
+			audio.addEventListener("error", () => {
+				URL.revokeObjectURL(url);
+			});
+		}
+	});
+}
+
+const getDuration = (chunks: ArrayBuffer[], songMetadata: any) => {
+	const chunkSize = chunks.map((chunk) => chunk.byteLength);
+	return (chunkSize.reduce((a, b) => a + b) * 8) / songMetadata.bitrate;
+};
 </script>
--- server-middleware/socket.ts
+++ server-middleware/socket.ts
@@ -1,4 +1,5 @@
 import { Server } from "socket.io";
+import ytdl from "ytdl-core";
 
 const io = new Server(3003, {
 	cors: {
@@ -8,6 +9,16 @@ const io = new Server(3003, {
 
 io.on("connection", async (socket) => {
 	console.log("Connection", socket.id);
+
+	socket.on("songRequest", async (url: string) => {
+		const songData = await ytdl.getInfo(url);
+		const formats = ytdl.filterFormats(songData.formats, "audioonly");
+		const audioFormat = ytdl.chooseFormat(formats, { quality: "highestaudio" });
+		socket.emit("songMetadata", audioFormat);
+
+		const stream = ytdl.downloadFromInfo(songData, { format: audioFormat });
+		stream.on("data", (chunk) => socket.emit("songData", chunk));
+	});
 
 	socket.on("disconnecting", () => {
 		console.log("disconnected", socket.id);
